27f08f3176ace1f9967a31a5a0ef8a5be0679ba0
vermavarun <vermavarun@outlook.com>
2026-02-20 21:14:04 +0530

    feat: Update Azure RM provider configuration to include resource group settings

--- LinearRegression/Infra/providers.tf
+++ LinearRegression/Infra/providers.tf
@@ -14,6 +14,10 @@ provider "azurerm" {
       purge_soft_delete_on_destroy    = false
       recover_soft_deleted_key_vaults = true
     }
+    
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
   }
   
   skip_provider_registration = false
